87e363429d54c4f3da9b5f6a8d015b8ddce638a6
John <caranayjohn21.student@gmail.com>
2025-01-30 14:19:35 -0500

    terrraform code

---
+++ dev/main.tf
@@ -0,0 +1,145 @@
+
+
+#----------------------------------------------------------
+# CLO835
+#
+# Build EC2 Instances
+#
+#----------------------------------------------------------
+
+#  Define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Data source for AMI id
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+# Data source for availability zones in us-east-1
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Data block to retrieve the default VPC id
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Define tags locally
+locals {
+  default_tags = merge(module.globalvars.default_tags, { "env" = var.env })
+  prefix       = module.globalvars.prefix
+  name_prefix  = "${local.prefix}-${var.env}"
+}
+
+# Retrieve global variables from the Terraform module
+module "globalvars" {
+  source = "../modules/globalvars"
+}
+
+# Create an IAM instance profile for the existing IAM role
+resource "aws_iam_instance_profile" "existing_profile" {
+  name = "LabRole"
+  role = "LabRole"
+}
+
+
+resource "aws_instance" "my_amazon" {
+  ami                         = data.aws_ami.latest_amazon_linux.id
+  instance_type               = lookup(var.instance_type, var.env)
+  key_name                    = aws_key_pair.my_key.key_name
+  vpc_security_group_ids      = [aws_security_group.my_sg.id]
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.existing_profile.name
+
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-Amazon-Linux"
+    }
+  )
+}
+
+
+# Adding SSH key to Amazon EC2
+resource "aws_key_pair" "my_key" {
+  key_name   = local.name_prefix
+  public_key = file("~/.ssh/${local.name_prefix}.pub")
+}
+
+# Security Group
+resource "aws_security_group" "my_sg" {
+  name        = "EC2-SG"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH from everywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from specific range"
+    from_port   = 8081
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-SG"
+    }
+  )
+}
+
+resource "aws_ecr_repository" "app_repo" {
+  name                 = "clo835-jcaranay-assignment1-app-repo"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-APP-REPO"
+    }
+  )
+}
+
+
+resource "aws_ecr_repository" "db_repo" {
+  name                 = "clo835-jcaranay-assignment1-db-repo"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-DB-REPO"
+    }
+  )
+}
---
+++ modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "John Caranay"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "ASSIGNMENT1"
+}
